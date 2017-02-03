6710a6ffbc2848a82001eaf5eb31417fbffc8d1c
jmaldonado93 <jorgem_93@hotmail.com>
2017-02-03 17:59:46 -0600

    Changes to ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,90 @@
 # When done, submit this entire file to the autograder.
 
-# Part 1
+# ==========================================================================
+# 								Part 1
+# ==========================================================================
 
 def sum arr
-  # YOUR CODE HERE
+	total = 0
+	if arr.size > 0
+		total = arr.reduce(:+)
+	end
+
+	return total
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+	total = 0
+	if arr.size > 0
+		arr_aux = arr.sort.last(2)
+		total = arr_aux.reduce(:+)
+	end
+	return total
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n? arr,n
+	result = arr.combination(2).any? {|x, y| x + y == n}
+	return result
 end
 
-# Part 2
+
+# ==========================================================================
+# 								Part 2
+# ==========================================================================
 
 def hello(name)
-  # YOUR CODE HERE
+	s = "Hello, " + name
+	return s
 end
 
+
 def starts_with_consonant? s
-  # YOUR CODE HERE
+	return false if s.empty?
+	
+	if /^[aeiou\W]/i.match(s) == nil
+		return true
+	else
+		return false
+	end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+	
+	if /^[0-1]+$/.match(s) == nil
+		return false
+	end
+	
+	s_aux = s.to_i(2)
+	if s_aux % 4 == 0
+		return true
+	else
+		return false
+	end
 end
 
-# Part 3
+
+# ==========================================================================
+# 								Part 3
+# ==========================================================================
 
 class BookInStock
-# YOUR CODE HERE
+	attr_accessor :isbn
+	attr_accessor :price
+
+	def initialize(isbn, price)
+		self.isbn = isbn
+		self.price = price
+		
+		if isbn.empty?
+			raise ArgumentError, "ISBN should not be empty."
+		end
+
+		if price <= 0
+			raise ArgumentError, "Price must be greater than zero."
+		end
+	end
+
+	def price_as_string
+		return "$%0.2f" % self.price
+	end
 end
